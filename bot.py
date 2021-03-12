c3ae316da7a809a7a1f4e0af204dcc8e1fcf39c9
romaincv12 <valasulth@gmail.com>
2021-03-12 09:22:33 +0100

    add the authentification to twitter

---
+++ bot.py
@@ -0,0 +1,11 @@
+import tweepy
+
+#Authenticate to twitter
+auth = tweepy.OAuthHandler("6z38xP3EE8KZ6uAyCnklAfZKZ",
+                            "NlNYEqCOpJDQ3I3NIexkXMELF3RTxsQLEiSBfsiojhiaHvhNe6")
+auth.set_access_token("2854605777-WCJpjK9W1G2Ab5a3oPNJGRAqEEyFGXOjeSflbRy", 
+                      "mz13qxaUsjicmCLUH3VxO3T186qmBNTlyluA7WldBlWLe")
+#Create API Object
+
+
+#Create a tweet
