7ecd33fbf4cbb5cfa11a9b9af738e814a24c45d5
romaincv12 <valasulth@gmail.com>
2021-03-12 09:25:25 +0100

    Create API Object

--- bot.py
+++ bot.py
@@ -6,6 +6,6 @@ auth = tweepy.OAuthHandler("6z38xP3EE8KZ6uAyCnklAfZKZ",
 auth.set_access_token("2854605777-WCJpjK9W1G2Ab5a3oPNJGRAqEEyFGXOjeSflbRy", 
                       "mz13qxaUsjicmCLUH3VxO3T186qmBNTlyluA7WldBlWLe")
 #Create API Object
-
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 #Create a tweet
